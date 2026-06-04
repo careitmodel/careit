d8f63cf9050d5166376a2032df0cdf4be2268a56
Charles-Simon Tima <contribute@careitmodel.org>
2026-06-04 21:09:25 +0200

    Update Docusaurus markdown link config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,7 +39,11 @@ const config = {
   projectName: 'careit', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
